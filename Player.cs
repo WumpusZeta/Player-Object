ca6748370794718c36ce5332c2a56de13d8c18d3
nsodoma <61425981+nsodoma@users.noreply.github.com>
2020-03-03 07:56:54 -0800

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WumpusTest
+{
+    public Player()
+
+    {
+
+        arrows = 5;
+
+        coins = 150;
+
+        turns = 0;
+
+    }
+
+
+
+    public bool useArrow()
+
+    {
+
+        if (arrows >= 1)
+
+        {
+
+            arrows -= 1;
+
+            return true;
+
+        }
+        else
+
+        {
+
+            return false;
+
+        }
+
+    }
+}
